d57487f6e8f20a3ff9c4393c92a43037b13206ef
Kogashinno <kogashinno@gmail.com>
2025-05-05 16:38:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 const config = {
-  channelAccessToken:"nLa1xPmavwxaxHUMMCNuDdeU2BHdT06rnAG5qmgLEfV+si2usanQFFHSscdy8a089J9e4aSPcdDoO47RrWlD7P9VQTT0woeHIY3z5SiV07ILsLruVNiENQJ5/FhwetHA4sXD0kdHoRDPCp/F2ed6RwdB04t89/1O/w1cDnyilFU=",
-  channelSecret:"f742c82dce71e7673676c92686cbfb69",
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
 const client = new line.Client(config);

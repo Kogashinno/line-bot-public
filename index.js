1ca301a9c7febd4662eb09e7443fec19805d1c03
Kogashinno <kogashinno@gmail.com>
2025-05-07 01:22:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 require('dotenv').config();
+
 console.log('SECRET:', process.env.LINE_CHANNEL_SECRET);
-const app = express();
-app.use(express.json());
 
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
-const client = new line.Client(config);
+const app = express();
 
+app.use(express.json()); 
 app.post('/webhook', line.middleware(config), async (req, res) => {
   try {
     const events = req.body.events;
-    const results = await Promise.all(events.map(handleEvent));
+    await Promise.all(events.map(handleEvent));
     res.sendStatus(200);
   } catch (err) {
     console.error('Error:', err);
     res.status(500).end();
   }
 });
+
 
 async function handleEvent(event) {
   console.log('受け取ったイベント:', JSON.stringify(event));
